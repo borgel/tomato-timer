03ac9ea5ecd7116798080f1986e4b47e5e144291
Kerry Scharfglass <iborgel@gmail.com>
2017-11-07 09:08:58 -0800

    Add EEPROM test

--- Firmware/Src/main.c
+++ Firmware/Src/main.c
@@ -14,6 +14,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define     ADDR_TOP       (0xA0)
+
 //FIXME move elsewhere
 extern I2C_HandleTypeDef hi2c1;
 
@@ -28,8 +30,34 @@ int main(void)
 
    //led_Init();
 
-   accelerometer_Init();
+   //accelerometer_Init();
    //accelerometer_TestStream();
+
+   HAL_StatusTypeDef stat;
+
+   iprintf("Starting EEPROM Test...\n");
+
+   uint8_t baddr = ADDR_TOP | (0x0 << 1);
+   uint8_t dataOut[] = "This is a test string!";
+   uint8_t dataIn[sizeof(dataOut)] = {};
+   stat = HAL_I2C_Mem_Read(&hi2c1, baddr, 0x0, 1, dataIn, sizeof(dataIn), 1000);
+   iprintf("Stat = 0x%x\n", stat);
+   iprintf("Read [%s]\n", dataIn);
+
+   // mask 3 block select bits in <<1
+   stat = HAL_I2C_Mem_Write(&hi2c1, baddr, 0x0, 1, dataOut, sizeof(dataOut), 1000);
+   iprintf("Stat = 0x%x\n", stat);
+   iprintf("Wrote [%s]\n", dataOut);
+
+   //TODO spin trying to initiate a write until we get an ACK (indicating the last
+   //write completed)
+
+   //TODO clock enable broken or something?
+   //HAL_Delay(100);
+
+   stat = HAL_I2C_Mem_Read(&hi2c1, baddr, 0x0, 1, dataIn, sizeof(dataIn), 1000);
+   iprintf("Stat = 0x%x\n", stat);
+   iprintf("Read [%s]\n", dataIn);
 
    return 0;
 }
